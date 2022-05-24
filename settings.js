a36f89320c06939ffdfe643ce695066fdc7572de
pa pan <gaga1945ab@gmail.com>
2022-05-24 09:15:23 +0700

    NarutoMD deploy

---
+++ settings.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//---------( APIKEY )---------//
+global.APIs = {
+zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+'https://zenzapis.xyz': 'BagasPrdn',
+}
+
+//---------( SETTING )---------//
+global.owner = ['62882005687229']
+global.premium = ['62882005687229']
+global.ownernomer = '62882005687229'
+global.ownername = '𝙽𝙰𝚁𝚄𝚃𝙾𝚇𝙳'
+global.botname = '𝙽𝙰𝚁𝚄𝚃𝙾𝙱𝙾𝚃'
+global.footer = '𝙽𝙰𝙽𝙳𝙰𝚇𝙳'
+global.thumb = fs.readFileSync('./image/fake.jpg')
+global.veloriy = fs.readFileSync('./image/kagura.mp4')
+global.sessionName = 'session'
+global.packname = '© 𝙽𝙰𝚁𝚄𝚃𝙾𝙱𝙾𝚃'
+global.author = '𝙽𝙰𝚁𝚄𝚃𝙾𝚇𝙳'
+global.sp = '⭔'
+
+//---------( INFO )---------//
+global.youtube = ' '
+global.ig = 'https://instagram.com/leomordbot'
+global.mygc = 'https://chat.whatsapp.com/D4Ww2QJRs1lEKqkniexY3u'
+global.myweb = ' '
+global.email = 'botleomord@gmail.com'
+global.region = 'indonesia'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.limitawal = {
+premium: "Infinity",
+free: 15,
+}
+
+//---------( RANDOM IMAGE )---------//
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//---------( MESSAGES )---------//
+global.mess = {
+success: '_Done._',
+admin: '_Fitur Khusus Admin Group!_',
+botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
+owner: '_Fitur Khusus Owner Bot_',
+group: '_Fitur Digunakan Hanya Untuk Group!_',
+private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sedang Di Proses..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+}
+
+//---------( PEMBATAS )---------//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
